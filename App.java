7a4528c1efadbfc2c44aa5169b99f13ccf0b4c16
Deniskin0110 <110915190+Deniskin0110@users.noreply.github.com>
2022-09-02 11:26:36 +0800

    Two calk

---
+++ App.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        while (true) {
+            System.out.print("calculate: ");
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            String result = Calculator.calc(input);
+            System.out.println("result: " + result);
+        }
+    }
+}
